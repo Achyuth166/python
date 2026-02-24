c1de8455f8acb56dacaa70613992a052c9e95cbb
Achyuth166 <kodiachyuth@gmail.com>
2026-02-24 21:58:21 +0530

    Refactor gender input handling and output messages in ifelse.py

--- ifelse.py
+++ ifelse.py
@@ -1,4 +1,12 @@
-if (1+1 == 2):
-    print("true")
+human = input("Enter your gender (boy/girl): ")
+if human == "boy":
+    print("go right side")
 else:
-    print("false")
+    print("go left side")
+
+# example 2
+gender = input("Enter your gender (boy/girl): ")
+if gender == "boy":
+    print("go right side.")
+else:
+    print("go left side.")
