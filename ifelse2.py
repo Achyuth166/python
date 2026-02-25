f586a3f2848588c36c1c6284c7ce7ebed012f6ae
Achyuth166 <kodiachyuth@gmail.com>
2026-02-25 21:21:51 +0530

    initial commit

---
+++ ifelse2.py
@@ -0,0 +1,12 @@
+human = input("Enter your gender (boy/girl): ")
+if human == "boy":
+    print("go right side")
+else:
+    print("go left side")
+
+# example 2
+gender = input("Enter your gender (boy/girl): ")
+if gender == "boy":
+    print("go right side.")
+else:
+    print("go left side.")
