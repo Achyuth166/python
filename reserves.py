ebe65dd0a255f15a8967ad91fbee25255e1ba976
Achyuth166 <kodiachyuth@gmail.com>
2026-02-28 13:39:43 +0530

    commit

---
+++ reserves.py
@@ -0,0 +1,36 @@
+s = "Achyuth"
+for i in range(6,-1,-1,):
+    print(s[i])
+print(len(s))# length of string
+
+k = [5,8,9,1,0]
+for i in range(4,-1,-1):
+    print(k[i]) 
+
+
+g = "aba"
+for i in range(2,-1,-1):
+    print(g[i]) 
+
+
+
+h = "abcd"
+ans = ""
+for i in range(3,-1,-1):
+    ans = ans + h[i]
+if (ans == h):
+    print("Palindrome")
+else:
+    print("Not a palindrome")
+# code indexing gives in string changes
+
+
+g = "sulli"
+ans = ""
+for i in range(len(g)-1,-1,-1):
+    ans = ans + g[i]
+if (ans == g):
+    print("yes,Palindrome")
+else:
+    print("Not a palindrome")
+    # correctly working code for palindrome check
