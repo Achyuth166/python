eaab3993a9ddc9cefeb04cd7b584d7f829a996a7
Achyuth166 <kodiachyuth@gmail.com>
2026-03-05 20:56:42 +0530

    commit

---
+++ double.py
@@ -0,0 +1,21 @@
+ki = [2,4,77,8,9]
+ans = 0
+for i in range(len(ki)):
+     val = ki[i]
+     ans = max(ans,val)
+print(ans)       
+
+for i in range(3):
+     print("hello world")
+     for j in range(2):
+          print("my self achyuth")
+     
+# double for loop
+
+mat = [[1,2,3,4],[5,6,7.8],[12,34,56,78]]
+
+r = len(mat)
+c = len(mat[0])
+
+for j in range(c):
+    print(mat[0][j])
