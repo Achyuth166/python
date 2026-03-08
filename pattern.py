62da5214239f8e426ea40f540cd0d4d9937fa406
Achyuth166 <kodiachyuth@gmail.com>
2026-03-08 11:01:25 +0530

    commit

--- pattern.py
+++ pattern.py
@@ -1,4 +1,100 @@
+#rectabgle dash pattern
 r = 3
+c = 3
+for i in range(r):
+    for j in range(c):
+        print("*", end="")
+        if j != c-1:
+            print("-", end=" ")
+    print()
+
+#star dash pattern
+r = 3
+c = 10
+for i in range(r):
+    for j in range(c):
+        print("*", end="")
+        if j != c-1:
+            print("-", end=" ")
+    print()
+
+#star Dash pattern
+r = 7
+c = 3
+for i in range(r):
+    for j in range(c):
+        print("*", end="")
+        if j != c-1:
+            print("-", end=" ")
+    print()
+
+#example 3
+n = 5
+for i in range(n):
+    for j in range(n):
+        print("*", end=" ")
+    print()
+
+# right shifted rectangle pattern
+r = 5
+c = 4
+for i in range(r):
+    for j in range(c):
+        if i == 0 or j == 0 or i == r-1 or j == c-1:
+            print("*", end="")
+        else:
+            print(" ", end="")
+            
+        if j != c-1:
+            print(" ", end="")
+    print()
+
+#reserve right shifted rectangle pattern
+r = 4
+c = 15
+for i in range(r):
+    for j in range(c):
+        if i == 0 or j == 0 or i == r-1 or j == c-1:
+            print("*", end="")
+        else:
+            print(" ", end="")
+
+        if j != c-1:
+            print(" ", end=" ")
+    print()
+
+
+#example
+r = 16
+c= 5
+c1 = r-1
+for i in range(r):
+    for j in range(c1-i):
+        print(" ", end="")
+    for k in range(c):
+        print("*", end="")
+    print()
+
+
+
+r = 16
+c= 5
+c1 = r-1
+for i in range(r):
+    for j in range(i):
+        print(" ", end="")
+    for k in range(c):
+        print("*", end="")
+    print()
+
+#examples
+r = 6
+c = 5
+c1 = r-1
+for i in range(r):
+    for j in range(c1 - i):
+        print("-", end="")
+    r = 3
 c = 3
 for i in range(r):
     for j in range(c):
@@ -33,10 +129,10 @@ for i in range(n):
     for j in range(n):
         print("*", end=" ")
     print()
-'''
 
 
-'''r = 5
+
+r = 5
 c = 4
 for i in range(r):
     for j in range(c):
@@ -47,9 +143,9 @@ for i in range(r):
             
         if j != c-1:
             print(" ", end="")
-    print()'''
+    print()
 
-'''r = 4
+r = 4
 c = 15
 for i in range(r):
     for j in range(c):
@@ -60,10 +156,9 @@ for i in range(r):
 
         if j != c-1:
             print(" ", end=" ")
-    print()'''
+    print()
 
-
-'''#example
+#example
 r = 16
 c= 5
 c1 = r-1
@@ -84,14 +179,17 @@ for i in range(r):
         print(" ", end="")
     for k in range(c):
         print("*", end="")
-    print()'''
+    print()  
 
-#examples
-r = 6
+# mountain pattern
+r = 5
 c = 5
 c1 = r-1
 for i in range(r):
     for j in range(c1 - i):
-        print("-", end="")
-    
+        print(" ", end="")
+    temp = i * 2 + 1
+    for k in range(temp):
+        print("*", end="")
+    print()
 
